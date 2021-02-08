845d31d6ac62c3f88eb92cc8b35fabed2638fb2d
HP <HP@JHONNY-PINEDA>
2021-02-07 19:12:27 -0600

    creando pagina Home

---
+++ QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
@@ -0,0 +1,22 @@
+﻿using QuieroPizza.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuieroPizza.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var Producto = new ProductoModel();
+            Producto.id = 1;
+            Producto.descripcion = "Pizza 6 quesos";
+
+            return View(Producto);
+        }
+    }
+}
