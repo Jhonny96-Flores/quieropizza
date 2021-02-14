bd2124b8fd3ac3ba8cba26e1e34ebdcb8d03dde2
HP <HP@JHONNY-PINEDA>
2021-02-13 23:45:10 -0600

    Creando tablas con html

---
+++ QuieroPizza/QuieroPizza.BL/ProductoBL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuieroPizza.BL
+{
+    public class ProductoBL
+    {
+        public List<Producto> ObtenerProductos()
+        {
+
+            var Producto1 = new Producto();
+            Producto1.Id = 1;
+            Producto1.Descripcion = "Pizza 6 quesos";
+            Producto1.Precio = 200;
+
+            var Producto2 = new Producto();
+            Producto2.Id = 2;
+            Producto2.Descripcion = "Pizza 4 Estaciones";
+            Producto2.Precio = 250;
+
+            var Producto3 = new Producto();
+            Producto3.Precio = 180;
+            Producto3.Id = 3;
+            Producto3.Descripcion = "Pizza Jamon y queso";
+
+            var listadeProducto = new List<Producto>();
+            listadeProducto.Add(Producto1);
+            listadeProducto.Add(Producto2);
+            listadeProducto.Add(Producto3);
+
+
+            return listadeProducto;
+        }
+    }
+}
--- QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
+++ QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
@@ -1,4 +1,4 @@
-﻿using QuieroPizza.Web.Models;
+﻿using QuieroPizza.BL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,23 +12,12 @@ namespace QuieroPizza.Web.Controllers
         // GET: Productos
         public ActionResult Index()
         {
-            var Producto1 = new ProductoModel();
-            Producto1.id = 1;
-            Producto1.descripcion = "Pizza 6 quesos";
+            var productosBL = new ProductoBL();
+            var listadeProductos = productosBL.ObtenerProductos();
 
-            var Producto2 = new ProductoModel();
-            Producto2.id = 2;
-            Producto2.descripcion = "Pizza 4 Estaciones";
 
-            var Producto3 = new ProductoModel();
-            Producto3.id = 3;
-            Producto3.descripcion = "Pizza Jamon y queso";
-
-            var listadeProducto = new List<ProductoModel>();
-            listadeProducto.Add(Producto1);
-            listadeProducto.Add(Producto2);
-            listadeProducto.Add(Producto3);
-            return View(listadeProducto);
+            return View(listadeProductos);
         }
     }
+
 }
