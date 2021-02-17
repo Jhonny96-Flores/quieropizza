5da18925ca663f70ad4e5d6e66032902f6dabdb3
HP <HP@JHONNY-PINEDA>
2021-02-16 20:01:43 -0600

    Creacion Data Base Local

--- QuieroPizza/QuieroPizza.BL/ProductoBL.cs
+++ QuieroPizza/QuieroPizza.BL/ProductoBL.cs
@@ -18,30 +18,7 @@ namespace QuieroPizza.BL
         }
         public List<Producto> ObtenerProductos()
         {
-            _contexto.Productos.ToList();
-
-            var Producto1 = new Producto();
-            Producto1.Id = 1;
-            Producto1.Descripcion = "Pizza 6 quesos";
-            Producto1.Precio = 200;
-
-            var Producto2 = new Producto();
-            Producto2.Id = 2;
-            Producto2.Descripcion = "Pizza 4 Estaciones";
-            Producto2.Precio = 250;
-
-            var Producto3 = new Producto();
-            Producto3.Precio = 180;
-            Producto3.Id = 3;
-            Producto3.Descripcion = "Pizza Jamon y queso ";
-
-            var listadeProducto = new List<Producto>();
-            listadeProducto.Add(Producto1);
-            listadeProducto.Add(Producto2);
-            listadeProducto.Add(Producto3);
-
-
-            return listadeProducto;
+            return _contexto.Productos.ToList(); 
         }
     }
 }
